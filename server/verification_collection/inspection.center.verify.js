b50f79d4f6ef4af979924a4f3bdac9fb77cbb0f1
saronwei <saronwei@live.com>
2016-04-13 10:37:15 +0800

    1. 在框架中增加了全局异常的捕获机制，避免因为未知异常而导致服务崩溃 2. 新增了参数验证机制，服务可灵活配置参数验证方式，简化逻辑核心代码避免在核心代码中出现过多的参数验证代码。 3. 新增了部分单例模块，实现数据在全代码范围内的共享使用。 4. 新增了异常编码配置，集中规整异常类型。 5. 新增了全局事件监听器，减少代码间的耦合关联。

---
+++ server/verification_collection/inspection.center.verify.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Saron on 2016/4/6.
+ */
+
+function InspectionCenterVerify() {
+
+    if (!(this instanceof InspectionCenterVerify)) {
+        condition = new InspectionCenterVerify();
+    }
+    else {
+        condition = this;
+    }
+
+    var condition;
+    var utils = require('util');
+    var BaseCondition = require('../../framework/verification/base.condition');
+    condition.prototype = new BaseCondition();
+
+    condition.prototype.Verify = function verify(data) {
+        return !utils.isNullOrUndefined(data)
+            && data.hasOwnProperty('center')
+            && !utils.isNullOrUndefined(data['center'])
+            && data['center'] != "";
+    };
+
+    condition.prototype.DoError = function doError() {
+        var msg = "Cannot run the inspection because of passing the incorrect center";
+        var error = condition.prototype.ErrorFactory.Machining("request_params_error", msg);
+        if (utils.isNullOrUndefined(error)) {
+            error = new Error(msg);
+            error.status = 412;
+            msg = null;
+        }
+        throw error;
+    };
+
+    return condition;
+}
+
+module.exports = InspectionCenterVerify;
---
+++ server/verification_collection/inspection.groupName.verify.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Saron on 2016/4/6.
+ */
+
+function InspectionGroupNameVerify() {
+
+    if (!(this instanceof InspectionGroupNameVerify)) {
+        condition = new InspectionGroupNameVerify();
+    }
+    else {
+        condition = this;
+    }
+
+    var condition;
+    var utils = require('util');
+    var BaseCondition = require('../../framework/verification/base.condition');
+    condition.prototype = new BaseCondition();
+
+    condition.prototype.Verify = function verify(data) {
+        return !utils.isNullOrUndefined(data)
+            && data.hasOwnProperty('groupName')
+            && !utils.isNullOrUndefined(data['groupName'])
+            && data['groupName'] != "";
+    };
+
+    condition.prototype.DoError = function doError() {
+        var msg = "Cannot run the inspection because of passing the incorrect group name";
+        var error = condition.prototype.ErrorFactory.Machining("request_params_error", msg);
+        if (utils.isNullOrUndefined(error)) {
+            error = new Error(msg);
+            error.status = 412;
+            msg = null;
+        }
+        throw error;
+    };
+
+    return condition;
+}
+
+module.exports = InspectionGroupNameVerify;
